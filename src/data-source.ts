83bcad0cb25e321461c67c0aba5fce4181f0d6d0
ssidartaa <odssy351@gmail.com>
2023-02-09 15:26:41 -0500

    fix: data-source entities and migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,12 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
 
+import Client from "./entities/clients.entity";
+import Contact from "./entities/contacts.entity";
+
+import { ClientsContactsEntities1675305213998 } from "./migrations/1675305213998-ClientsContactsEntities";
+import { UpdateCascadeClient1675880138232 } from "./migrations/1675880138232-UpdateCascadeClient";
+
 const AppDataSource = new DataSource(
   process.env.NODE_ENV === "test"
     ? {
@@ -18,8 +24,11 @@ const AppDataSource = new DataSource(
         database: process.env.POSTGRES_DB,
         logging: true,
         synchronize: false,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
+        entities: [Client, Contact],
+        migrations: [
+          ClientsContactsEntities1675305213998,
+          UpdateCascadeClient1675880138232,
+        ],
       }
 );
 
